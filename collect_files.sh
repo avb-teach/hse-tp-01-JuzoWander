4ff9e84a3315d23d5b848f0485b3b7a10d093fd1
JuzoWander <colchesty@yandex.ru>
2025-04-28 19:31:28 +0300

    depth_2

--- collect_files.sh
+++ collect_files.sh
@@ -5,29 +5,27 @@
 
 INPUT_DIR=$1
 OUTPUT_DIR=$2
+max_depth = 
 if [$# -ne 2]; then
   max_depth=$3
+  ((max_depth--))
+  ((max_depth--))
 fi
-if [ -z "$max_depth" ];then
-  for file in $(find "$INPUT_DIR" -type f); do
+
+for file in $(find "$INPUT_DIR" -type f); do
   name=$(basename "$file")
-  new_file_dir="$2/$name"
+  path=${file#$INPUT_DIR/}
+  path=${path%/*}
+  if [ -n "$max_depth" ];then
+    while [ $(echo "$path" | tr -cd '/' | wc -c) -gt $max_depth ]; do
+      path=${path#*/}
+    done
+  fi
+  new_file_dir="$OUTPUT_DIR/$path/$name"
   ind=1
   while [-f "$new_file_dir"];do
-    new_file_dir="$OUTPUT_DIR/${name%.*}$ind.${name##*.}"
+    new_file_dir="$OUTPUT_DIR/$path/${name%.*}$ind.${name##*.}"
     ((ind++))
   done
   cp "$file" "$new_file_dir"
-  done
-else
-  for file in $(find "$INPUT_DIR" -maxdepth "$max_depth" -type f); do
-  name=$(basename "$file")
-  new_file_dir="$2/$name"
-  ind=1
-  while [-f "$new_file_dir"];do
-    new_file_dir="$OUTPUT_DIR/${name%.*}$ind.${name##*.}"
-    ((ind++))
-  done
-  cp "$file" "$new_file_dir"
-  done
-fi
+done
