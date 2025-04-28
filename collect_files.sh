a6e5637426fc0b0404ae13886e73b44fddea8aeb
JuzoWander <colchesty@yandex.ru>
2025-04-28 18:30:40 +0300

    copy_2

--- collect_files.sh
+++ collect_files.sh
@@ -6,10 +6,8 @@ fi
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-files = $(find "$INPUT_DIR" -type f)
-
-for file in "${files[@]}"; do
-    name=$(basename "$file")
-    new_file_dir="$2/$name"
-    cp "$file" "$new_file_dir"
+for file in $(find "$INPUT_DIR" -type f); do
+  name=$(basename "$file")
+  new_file_dir="$2/$name"
+  cp "$file" "$new_file_dir"
 done
